1408215dc648a9f3e18712a5498cb3b6db99197d
Fernando Aldaz <118292813+Josxx24@users.noreply.github.com>
2025-05-27 20:39:46 -0500

    Initial commit: all project files added

---
+++ assets/js/01-javascript.js
@@ -0,0 +1,13 @@
+console.log("Mundo");
+
+//tipos de variables 35
+// (mutable)
+let nombre = "Adrian";
+nombre = "Carolina";
+// nombre = 1;
+// nombre = function(){
+//     return 1;
+// };
+// (Inmutables)
+const cedula = "1111111111";
+//cedula = 1; Error: Assignment to constant 
