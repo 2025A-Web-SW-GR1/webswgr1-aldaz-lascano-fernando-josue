83792dac9259fe88d8033ab97de56c1631e58f2f
Fernando Aldaz <118292813+Josxx24@users.noreply.github.com>
2025-06-22 08:43:19 -0500

    Examen-FernandoAldaz

--- 02 - servidor/ejemplo-serviodor/src/app.module.ts
+++ 02 - servidor/ejemplo-serviodor/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CasaController } from '../Examen-FernandoAldaz/casa.controller';
+import { CasaService } from '../Examen-FernandoAldaz/casa.service'; 
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CasaController],
+  providers: [AppService, CasaService],
 })
 export class AppModule {}
